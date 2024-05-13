18d1bfeb4255779504b6c80efc48ad7954b99851
RLRGC <120052136+RLRGC@users.noreply.github.com>
2024-05-13 16:58:48 +0200

    Carte qui marche §!!!!!!!!

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -9,10 +9,11 @@ function RDC(){
 	
 }
 function Etage1(){
-	document.querySelector('#Etage1 tbody').style.display= 'inherit';	
-	document.querySelector('#RDC').style.display= 'none';
-	document.querySelector('#Etage2').style.display= 'none';
+	document.querySelector('#Etage1').style.display = 'inherit';
+	document.querySelector('#RDC').style.display= 'none'; //Supprime l'étage RDC
+	document.querySelector('#Etage2').style.display= 'none';//Supprime l'étage 2
 }
+
 function Etage2(){
 	document.querySelector('#Etage2').style.display= 'inherit';	
 	document.querySelector('#Etage1').style.display= 'none';
