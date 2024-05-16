478255a7ec313a83882667380e6a45c08d2e9bd9
RLRGC <120052136+RLRGC@users.noreply.github.com>
2024-05-16 09:29:57 +0200

    Ajout des alertes

---
+++ JavaScript/Alert.js
@@ -0,0 +1,15 @@
+function Cabine1(){
+	alert("CABINE 1 \n \n Salle occupé : True/False \n Température : 23°C \n Taux de CO2 : 0.08%");
+}
+function Cabine23(){
+	alert("CABINE 2 ET 3 \n \n Salle occupé : True/False \n Température : 23°C \n Taux de CO2 : 0.08%");
+}
+function Cabine45(){
+	alert("CABINE 4 ET 5 \n \n Salle occupé : True/False \n Température : 22.99°C \n Taux de CO2 : 0.0788889987%");
+}
+function Cabine5(){
+	alert("CABINE 5 \n Salle occupé : True \n Température : 22.05 \n Taux de CO2 : 0.08999999999");
+}
+function Cabine34(){
+	alert("CABINE 3 ET 4 \n \n Salle occupé : True/False \n Température : 23°C \n Taux de CO2 : 0.08%");
+}
