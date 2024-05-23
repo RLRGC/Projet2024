34ce2ac3443daf3e2d1a44a748d2125a15d316ff
RLRGC <120052136+RLRGC@users.noreply.github.com>
2024-05-23 08:01:17 +0200

    Finition

--- JavaScript/Alert.js
+++ JavaScript/Alert.js
@@ -1,3 +1,6 @@
+function precise(x) {
+	return x.toPrecision(4);
+  }
 function Cabine1(){
 	alert("CABINE 1 \n \n Salle occupé : True/False \n Température : 23°C \n Taux de CO2 : 0.08%");
 }
@@ -12,4 +15,26 @@ function Cabine5(){
 }
 function Cabine34(){
 	alert("CABINE 3 ET 4 \n \n Salle occupé : True/False \n Température : 23°C \n Taux de CO2 : 0.08%");
+}
+function ReeducationKine2(){
+	alert("Salle de Réeducation Kinésithérapeute \n \n Personnes dans la salle : 9 personnes \n Température : 23°C \n Taux de CO2 : 0.08%")
+}
+function Terrasse(){
+	let personnes = Math.floor(Math.random() * 6);
+	alert("Terrasse \n \n Personnes : " + personnes );
+}
+function Kinésithérapeute2(){
+	alert("Salles d'attentes \n  Personnes : " + Math.floor(Math.random() * 7) + " \n Temperature : 22.07°C \n Taux de C02 : 0.068");
+}
+function Osteopathe(){
+	alert("Salle Ostéo \n Personnes : " + Math.floor(Math.random()*3) + " \n Température : " + precise(Math.floor(20+Math.random() * 4)+Math.random(5)) +  "°C \n Taux de CO2 : 0.070" )
+}
+function EspaceDetentePerso(){
+	alert("Espace de détente perso \n Personnes : " + Math.floor(Math.random()*13) + "\n Température : "  + precise(Math.floor(20+Math.random() * 4)+Math.random(5)) + "°C \n Taux de CO2 : 0.072" )
+}
+function Infirmier(){
+	alert("Infirmier \n Personne : " + Boolean(Math.floor(Math.random()*3)-1) + "\n Température : " + precise(Math.floor(20+Math.random() * 4)+Math.random(5)) + "°C \n Taux de CO2 : 0.071" );
+}
+function Sophrologue(){
+	alert("Sophrologue \n Personne : " + Boolean(Math.floor(Math.random()*3)-1) + "\n Température : " + precise(Math.floor(20+Math.random() * 4)+Math.random(5)) + "°C \n Taux de CO2 : 0.071" );
 }
